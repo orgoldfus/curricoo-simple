364ad8e2f327279ff8b71921dd92447f3bcb8bee
Or Goldfus <orgoldfus@Ors-MacBook-Pro.local>
2018-10-16 14:26:47 +0300

    add and configure react client

--- app.js
+++ app.js
@@ -30,8 +30,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/client/build/')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 app.use('/', curricoosRouter);
 
 // catch 404 and forward to error handler
